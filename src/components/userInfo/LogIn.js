c8f61a81ea6e614faa8d5d797d7d1c0f286fbde6
taghreedmn <taghreed.mn18@gmail.com>
2024-11-06 05:57:42 +0300

    Complete login form

--- src/components/userInfo/LogIn.js
+++ src/components/userInfo/LogIn.js
@@ -1,7 +1,10 @@
 import React, {useState} from 'react'
 import './SignForm.css';
+import { Link, useNavigate } from "react-router-dom";
+import axios from 'axios';
 
 export default function LogIn() {
+    const navigate = useNavigate();
     const [formData, setFormData] = useState({
         email: "",
         password: ""
@@ -10,13 +13,43 @@ export default function LogIn() {
         setFormData({ ...formData, [e.target.name]: e.target.value })
     };
     const handleSubmit = (e) => {
+        const logUrl = "http://localhost:5125/api/v1/Person/signIn";
+
+        const dataToSend = {
+            personEmail: formData.email,
+            personPassword: formData.password,
+        };
+
+        axios.post(logUrl, dataToSend)
+            .then((res) => {
+                console.log(res, "response from post")
+                if (res.status === 200) {
+                    navigate("/");
+                }
+            })
+            .catch((err) => {
+                console.log(err)
+                if (err === 400) {
+
+                    if (err.response.data.personEmail) {
+                        alert(err.response.data.personEmail[0]);
+                        return;
+                    }
+                    if (err.response.data.personPassword) {
+                        alert(err.response.data.personPassword[0]);
+                        return;
+                    }
+
+
+                }
+            });
         e.preventDefault();
         console.log("Login Data: ", formData)
     };
   return (
       <div className='sign-container'>
           <h2>Login</h2>
-          <form onSubmit={handleSubmit}>
+          <form >
               <input
                   type="email"
                   name="email"
@@ -33,7 +66,7 @@ export default function LogIn() {
                   onChange={handleChange}
                   required
               />
-              <button type='Submit' >Login</button>
+              <button type='Submit' onClick={handleSubmit}>Login</button>
 
           </form>
     </div>
